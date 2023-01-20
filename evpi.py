e11598de24956c679ee093d9561e98c4a95d3041
Johannes Kopton <johannes@kopton.org>
2023-01-20 18:55:41 +0100

    Refactor evpi coding style.

--- evpi.py
+++ evpi.py
@@ -1,6 +1,39 @@
 import numpy as np
 
 
+def ev_pi_bins(x, y, n_bins):
+    """Loops through the bins of a histogram over the input and yields the
+        sum of the respective output samples if positive, zero otherwise.
+
+        Parameters
+        ----------
+        x : array_like
+            Input samples.
+        y : array_like
+            Output samples.
+        n_bins : int
+            Number of histogram bins.
+
+        Yields
+        ------
+        int
+            Sum of optimal decision option output for each bin. Can be
+            interpreted as the expected value weighted with the number of
+            samples in the bin.
+        """
+
+    # divide the estimate samples into `n_bins`
+    hist_bins = np.histogram(x, bins=n_bins)[1]
+
+    for i in range(n_bins):
+        # create a binary mask for samples inside the bin
+        subset_mask = (hist_bins[i] <= x) * (x < hist_bins[i+1])
+        # apply this mask on the output
+        y_subset = y[subset_mask]
+        # return sum of this output if positive, otherwise zero
+        yield max(np.sum(y_subset), 0)
+
+
 def evpi(x, y, n_bins):
     """Calculates EVPI for one estimate and one decision criterion.
     EVPI means "Expected Value of Perfect Information" and can be described
@@ -30,38 +63,6 @@ def evpi(x, y, n_bins):
 
     # expected maximum value
     emv = max(ev_no, ev_yes)
-
-    def ev_pi_bins(x, y, n_bins):
-        """Loops through the bins of a histogram over the input and yields the
-        sum of the respective output samples if positive, zero otherwise.
-
-        Parameters
-        ----------
-        x : array_like
-            Input samples.
-        y : array_like
-            Output samples.
-        n_bins : int
-            Number of histogram bins.
-
-        Yields
-        ------
-        int
-            Sum of optimal decision option output for each bin. Can be
-            interpreted as the expected value weighted with the number of
-            samples in the bin.
-        """
-
-        # divide the estimate samples into `n_bins`
-        hist_bins = np.histogram(x, bins=n_bins)[1]
-
-        for i in range(n_bins):
-            # create a binary mask for samples inside the bin
-            subset_mask = (hist_bins[i] <= x) * (x < hist_bins[i+1])
-            # apply this mask on the output
-            y_subset = y[subset_mask]
-            # return sum of this output if positive, otherwise zero
-            yield max(np.sum(y_subset), 0)
 
     ev_pi = sum(ev_pi_bins(x, y, n_bins)) / len(x)
     evpi = ev_pi - emv

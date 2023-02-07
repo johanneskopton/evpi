8a3c7e67ed0f426ab9aa1b1211c85f9405bb3790
Johannes Kopton <johannes@kopton.org>
2023-02-07 15:25:06 +0100

    Set up python package structure.

---
+++ python/README.md
@@ -0,0 +1,9 @@
+# Python Implementation of EVPI
+
+This is an implementation of the expected value of perfect information (EVPI) written in Python using Numpy with no custom C extensions.
+
+## Installation
+
+```sh
+pip install .
+```
---
+++ python/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    # Utility function to read the README file
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="py_evpi",
+    version="1.0.0",
+    author="Johannes Kopton",
+    author_email="johannes@kopton.org",
+    description=("EVPI implementation in Python using Numpy."),
+    keywords="bayesian information economics value decision analysis",
+    url="https://github.com/johanneskopton/evpi",
+    packages=["py_evpi"],
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 3"
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy ~= 1.19",
+    ],
+    extras_require={
+        "dev": [
+            "autopep8",
+            "flake8"
+        ],
+    },
+)

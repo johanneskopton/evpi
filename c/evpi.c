52e09c0a2e89a81fc4e12052cf59abd3553b1e43
Johannes Kopton <johannes@kopton.org>
2023-03-01 22:41:28 +0100

    Apply automatic formatting using clangd.

--- c/evpi.c
+++ c/evpi.c
@@ -1,25 +1,23 @@
+#include "evpi.h"
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include "evpi.h"
 
-bool is_const(const double array[], int n)
-{   
+bool is_const(const double array[], int n) {
     const int a0 = array[0];
-    for (int i = 1; i < n; i++)      
-    {         
+    for (int i = 1; i < n; i++) {
         if (array[i] != a0)
             return false;
     }
     return true;
 }
 
-double* mean_samples(double** matrix, size_t n_samples, size_t n_vars){
+double* mean_samples(double** matrix, size_t n_samples, size_t n_vars) {
     double* result = malloc(n_vars * sizeof(double));
-    for (size_t j=0; j<n_vars; j++){
+    for (size_t j = 0; j < n_vars; j++) {
         double sum = 0;
-        for (size_t i=0; i<n_samples; i++){
+        for (size_t i = 0; i < n_samples; i++) {
             sum += matrix[i][j];
         }
         result[j] = sum / n_samples;
@@ -27,28 +25,28 @@ double* mean_samples(double** matrix, size_t n_samples, size_t n_vars){
     return result;
 }
 
-double maximum(double* vector, size_t length){
+double maximum(double* vector, size_t length) {
     double result = vector[0];
-    for (size_t i = 1; i<length; i++){
-        if (vector[i] > result){
+    for (size_t i = 1; i < length; i++) {
+        if (vector[i] > result) {
             result = vector[i];
         }
     }
     return result;
 }
 
-double evppi(double* x, double** y, size_t n_samples, size_t n_options){
+double evppi(double* x, double** y, size_t n_samples, size_t n_options) {
     /*
     Check if there is variance (-> uncertainty) in the input.
     If there is none, then there can't be any value in reducing it.
-    */ 
-    if (is_const(x, n_samples)){
+    */
+    if (is_const(x, n_samples)) {
         return 0;
     }
-    
+
     // Use cubic root of sample number as default bin number.
     unsigned int n_bins = (unsigned int)cbrt(n_samples);
-    
+
     // expected maximum value
     double emv = maximum(mean_samples(y, n_samples, n_options), n_options);
     return 1;
--- c/read_csv.c
+++ c/read_csv.c
@@ -1,50 +1,48 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<stdbool.h>
 #include "evpi.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAXCHAR 100
 
-unsigned int count_samples(FILE *fp)
-{
+unsigned int count_samples(FILE* fp) {
     unsigned int samplecount = 0;
     for (char c = getc(fp); c != EOF; c = getc(fp))
-        if (c == '\n') samplecount++;
+        if (c == '\n')
+            samplecount++;
     rewind(fp);
-    return samplecount-1;
+    return samplecount - 1;
 }
 
-unsigned int count_vars(FILE *fp)
-{
+unsigned int count_vars(FILE* fp) {
     unsigned int varcount = 0;
     for (char c = getc(fp); c != '\n'; c = getc(fp))
-        if (c == ',') varcount++;
+        if (c == ',')
+            varcount++;
     rewind(fp);
     return varcount;
 }
 
-double** get_vals(FILE *fp, size_t n_samples, size_t n_vars)
-{
+double** get_vals(FILE* fp, size_t n_samples, size_t n_vars) {
     // set up 2d array as pointer of pointers
-    double *data = malloc(n_samples * n_vars * sizeof(double));
-    double **result = malloc(n_samples * sizeof(double*));
-    for(size_t i=0; i<n_samples; i++) result[i] = data + i*n_vars;
+    double* data = malloc(n_samples * n_vars * sizeof(double));
+    double** result = malloc(n_samples * sizeof(double*));
+    for (size_t i = 0; i < n_samples; i++)
+        result[i] = data + i * n_vars;
 
     // parse csv into this array
     char row[MAXCHAR];
-    char *token;
+    char* token;
     size_t i, j;
     i = 0;
     fgets(row, MAXCHAR, fp); // skip first row (header)
-    while (feof(fp) != true)
-    {
+    while (feof(fp) != true) {
         fgets(row, MAXCHAR, fp);
         token = strtok(row, ",");
         token = strtok(NULL, ","); // skip first col (index)
         j = 0;
-        while(token != NULL)
-        {
+        while (token != NULL) {
             result[i][j] = atof(token);
             token = strtok(NULL, ",");
             j++;
@@ -54,36 +52,34 @@ double** get_vals(FILE *fp, size_t n_samples, size_t n_vars)
     return result;
 }
 
-double* get_col(double** matrix, size_t n_samples, size_t n_vars, size_t col_idx)
-{
+double* get_col(double** matrix, size_t n_samples, size_t n_vars,
+                size_t col_idx) {
     double* column = malloc(n_samples * sizeof(double));
-    for(size_t i=0; i<n_samples; i++){
+    for (size_t i = 0; i < n_samples; i++) {
         column[i] = matrix[i][col_idx];
     }
     return column;
 }
 
-double** parse_csv(char* path, size_t* n_samples, size_t* n_vars)
-{
-    FILE *fp;
-    fp = fopen(path,"r");
+double** parse_csv(char* path, size_t* n_samples, size_t* n_vars) {
+    FILE* fp;
+    fp = fopen(path, "r");
     *n_samples = count_samples(fp);
     *n_vars = count_vars(fp);
     double** matrix = get_vals(fp, *n_samples, *n_vars);
     fclose(fp);
-    return(matrix);
+    return (matrix);
 }
 
-int main()
-{
+int main() {
     size_t n_samples_x, n_samples_y, n_vars_x, n_vars_y;
     double** x = parse_csv("../test_data/x.csv", &n_samples_x, &n_vars_x);
     double** y = parse_csv("../test_data/y.csv", &n_samples_y, &n_vars_y);
     double* x1 = get_col(x, n_samples_x, n_vars_x, 1);
     // double* y2 = get_col(y, n_samples_y, n_vars_y, 2);
-    
+
     double evpi_res = evppi(x1, y, n_samples_x, n_vars_y);
     printf("%f\n", evpi_res);
-    
+
     return 0;
 }

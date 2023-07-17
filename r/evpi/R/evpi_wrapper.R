fe5d8369c6dbbc141fe232f4cee904480e8a50a1
Johannes Kopton <kopton@uni-bonn.de>
2023-07-17 20:54:02 +0200

    Make R wrapper a proper package with some documentation.

--- r/evpi/R/evpi_wrapper.R
+++ r/evpi/R/evpi_wrapper.R
@@ -1,3 +1,15 @@
+#' Calculate Expected Value of Perfect Parameter Information (EVPPI) for
+#' multiple input variables and multiple decision options.
+#'
+#' @param x Monte Carlo samples from the probability distribution of the
+#' considered parameter (aka estimates aka "input" variables). Columns are
+#' variables, rows are samples.
+#' @param y The respective utility (aka outcome) samples calculated using the
+#' estimate samples of x. This criterion is considered to be the (only)
+#' decision criterion for a risk-neutral decision maker, that chooses
+#' the option with the highest expected utility. Samples are rows,
+#' decision options are columns.
+#' @return Vector of EVPPI values in the order of the columns of `x`.
 multi_evppi <- function(x, y){
   x = data.matrix(x)
   y = data.matrix(y)
@@ -8,6 +20,18 @@ multi_evppi <- function(x, y){
   return(result)
 }
 
+#' Calculate Expected Value of Perfect Parameter Information (EVPPI) for
+#' multiple input variables and two decision options (yes/no).
+#'
+#' @param x Monte Carlo samples from the probability distribution of the
+#' considered parameter (aka estimates aka "input" variables). Columns are
+#' variables, rows are samples.
+#' @param y The respective utility (aka outcome) samples calculated using the
+#' estimate samples above. This utility is considered to be the (only)
+#' decision criterion for a risk-neutral decision maker facing a binary
+#' decision, so that a positive expected value will lead to `yes` and a
+#' negative one to `no`.
+#' @return Vector of EVPPI values in the order of the columns of x.
 binary_multi_evppi <- function(x, y){
   x = data.matrix(x)
   y = data.matrix(y)
